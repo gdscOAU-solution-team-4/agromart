56bcd4c9cfd59343b79f54387507ff82ec9fbedb
Codebox124 <rajcodes733@gmail.com>
2023-02-21 06:52:59 -0800

    added tailwind css pls update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,12 @@ import banner from '../assets/image.png'
 export default function App() {
     return (
      <div className="hero-bg bg-contain">
+        <div className='max-w-6xl m-auto'>
+            <div>
+                <h2 className='text-3xl content-center flex self-center'>Choose the best healthier way of life</h2>
+            </div>
+
+        </div>
       
       
      </div>
