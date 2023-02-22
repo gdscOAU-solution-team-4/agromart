f6e747b998607d19ae11fac8b527de3f14bef92b
Codebox124 <rajcodes733@gmail.com>
2023-02-22 11:08:40 -0800

    heropage done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Offer from "./components/Offer";
+import About from "./components/About";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Hero />
       <Offer />
+      <About />
       
       
       
