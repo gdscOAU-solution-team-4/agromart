8e63bfdc91a1620e323e8e8d3b3c526532ffbd15
Codebox124 <rajcodes733@gmail.com>
2023-02-21 19:01:15 -0800

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl text-center text-red-900 font-bold underline">
-        Hello world!
-      </h1>
-      <Outlet />
-      <div>footer</div>
-    </>
+    <div>
+      <Header />
+      
+      
+      
+    </div>
   );
 }
