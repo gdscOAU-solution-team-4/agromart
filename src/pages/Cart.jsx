45c2ba5bf6e96c0762893690581f5c3dd03f0971
Jae <gideonoakinduro@gmail.com>
2023-02-27 06:10:11 +0100

    bootstrapped cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import SectionFragment from '../components/UI/SectionFragment'
+
+export default function Cart() {
+  return (
+    <section id='cart' className='min-h-screen bg-white-100'>
+      <SectionFragment>
+              <h1>Order Details</h1>
+              
+              <div>
+                  
+              </div>
+      </SectionFragment>
+    </section>
+  )
+}
