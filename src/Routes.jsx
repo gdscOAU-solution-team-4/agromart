2b797cc8b9234a2248ea126b00608c35bf4f8fe2
Jae <gideonoakinduro@gmail.com>
2023-02-27 04:22:52 +0100

    feat: added route to recover pasword

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -7,6 +7,7 @@ import {
 import App from './App'
 import BuyerDash from './pages/BuyerDash'
 import ErrorPage from './pages/ErrorPage'
+import ForgotPassword from './pages/forgotPassword'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
         <Route path='/register' element={<SignUp />} />
         <Route path='/login' element={<SignIn />} />
         <Route path='/shop' element={<BuyerDash />} />
+        <Route path='/forgotten-password' element={<ForgotPassword />} />
       </Route>
     </Route>
   )
