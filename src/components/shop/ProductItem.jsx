074dbec7cbd85d177fcaa5167cdbec99ceef0ccb
Jae <gideonoakinduro@gmail.com>
2023-03-01 11:59:35 +0100

    lil fix

--- src/components/shop/ProductItem.jsx
+++ src/components/shop/ProductItem.jsx
@@ -9,18 +9,18 @@ export default function ProductItem({
   imgUrl,
 }) {
   return (
-    <Card className='cursor-pointer rounded-2xl bg-white drop-shadow-[0px_4px_10px_rgba(0,0,0,0.25)] transition-transform ease-in hover:translate-y-2'>
+    <Card className='flex cursor-pointer flex-col justify-between rounded-2xl bg-white drop-shadow-[0px_4px_10px_rgba(0,0,0,0.25)] transition-transform ease-in hover:translate-y-2'>
       <h5 className='max-w-max rounded-lg bg-[#274c5b] p-2 text-center font-openSan text-sm font-semibold text-white md:text-base'>
         {category}
       </h5>
-      <div>
+      <div className='grid w-full place-items-center'>
         <img src={imgUrl} alt={name} />
       </div>
       <div>
         <p className='font-roboto text-base font-semibold text-[#274c5b] md:text-xl'>
           {name}
         </p>
-        <hr />
+        <hr className='my-3' />
         <div className='flex justify-between'>
           <div className='flex gap-2 font-openSan font-semibold'>
             <p className='text-[#B8B8B8] line-through'>${price}</p>
