2c6552c0e1d56a5fec9d5ba13158cb273def42b3
dev-phantom <godsfavour1975@gmail.com>
2023-03-03 16:01:35 +0100

    hoping this work

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,26 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
-      }
+      },
+      manifest: {
+        name: 'Agromart site',
+        short_name: 'Agromart',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
      })
   ],
   resolve: {
