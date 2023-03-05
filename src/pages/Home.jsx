38ae2f91569e8adc5d346af21904f80bc74f9bed
Jae <gideonoakinduro@gmail.com>
2023-03-05 15:00:47 +0100

    static products section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 import About from '../components/home/About'
 import Hero from '../components/home/Hero'
 import Offer from '../components/home/Offer'
+import Products from '../components/home/Products'
 import DefaultLayout from '../layout/defaultLayout'
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <main>
+      <main className='bg-[#F9F8F8]'>
         <Hero />
         <Offer />
         <About />
+        <Products />
       </main>
     </DefaultLayout>
   )
